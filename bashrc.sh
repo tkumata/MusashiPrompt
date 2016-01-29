7878c52602561559980a26ad8dc79033a47662b0
tkumata <tkumata@gmail.com>
2016-01-29 23:07:01 +0900

    ESC の挙動を関数化

--- bashrc.sh
+++ bashrc.sh
@@ -1,6 +1,22 @@
 # bash 限定
 # HISTCONTROL に ignore* 入れない
 # PIPESTATUS 位置重点
+function codeCondition () {
+    local code=$1
+    
+    if [ ${code} -eq 141 ]; then
+        echo -n "パイプの後に head を使用していたら無視して構いません。"
+    elif [ ${code} -eq 127 ]; then
+        echo -n "コマンドのスペルを確認してください。"
+    elif [ ${code} -gt 0 -a ${code} -lt 256 ]; then
+        echo -n "引数もしくはコマンドを確認してください。"
+    elif [ ${code} -eq 0 ]; then
+        echo -n "正常値です。"
+    else
+        echo -n "不明な終了ステータスコードです。"
+    fi
+}
+
 function musashi () {
     local status=$(echo ${PIPESTATUS[@]})
     local statusArray=($(echo $status))
@@ -14,12 +30,12 @@ function musashi () {
         dummy=""
     else
         cmd=$(echo "${hist}" | tail -1)
-        #echo ""
         echo -n "〝${AM[$NO]}〟: ${cmd} を実行しました。終了ステータスコードは"
         
         if [ "${#statusArray[@]}" -gt 1 ]; then
             echo "それぞれ"
             i=1
+            
             for s in ${status}; do
                 if [ "${#statusArray[@]}" -eq "$i" ]; then
                     local echoopt="-ne"
@@ -27,25 +43,14 @@ function musashi () {
                     local echoopt="-e"
                 fi
                 
-                if [ ${s} -eq 141 ]; then
-                    echo ${echoopt} "$i 番目、${s} です。$(( $i+1 )) 番目で head を使用していたら無視して構いません。"
-                elif [ ${s} -eq 127 ]; then
-                    echo ${echoopt} "$i 番目、${s} です。コマンドのスペルを確認してください。"
-                elif [ ${s} -gt 0 -a ${s} -lt 256 ]; then
-                    echo ${echoopt} "$i 番目、${s} です。引数もしくはコマンドを確認してください。"
-                elif [ ${s} -eq 0 ]; then
-                    echo ${echoopt} "$i 番目、${s} です。正常値です。"
-                else
-                    echo ${echoopt} "不明な終了ステータスコードです。"
-                fi
+                coderesult=$(codeCondition ${s})
+                echo ${echoopt} "$i 番目、${s} です。${coderesult}"
+                
                 i=$(( $i+1 ))
             done
         else
-            if [ ${status} -gt 0 ]; then
-                echo -n " ${status} です。コマンドや引数を確認してください。"
-            else
-                echo -n " ${status} です。正常値です。"
-            fi
+            coderesult=$(codeCondition ${status})
+            echo -n " ${status} です。${coderesult}"
         fi
         
         echo "――以上"
