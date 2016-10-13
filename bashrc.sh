c1f54c1a7b9305dc237417a27ce21c614a423d44
tkumata <tkumata@gmail.com>
2016-10-13 23:30:10 +0900

    change indent width

--- bashrc.sh
+++ bashrc.sh
@@ -80,11 +80,11 @@ function detect_distro () {
       ;;
     Linux)
       if [ -f /etc/os-release ]; then
-          . /etc/os-release
-          echo "${NAME}"
+        . /etc/os-release
+        echo "${NAME}"
       elif [ -f /etc/lsb-release ]; then
-          . /etc/lsb-release
-          echo "${DISTRIB_ID}"
+        . /etc/lsb-release
+        echo "${DISTRIB_ID}"
       fi
       ;;
     *)
@@ -95,98 +95,97 @@ function detect_distro () {
 
 # PIPESTATUS 位置重点
 function musashi () {
-    local status=$(echo ${PIPESTATUS[@]})
-    local status_array=($(echo $status))
-    local hist=$(history | tail -10 | awk '{$1=""; print $0}' | awk '{sub(/^[ \t]+/, "")}1')
-    
-    local automaton_names=("武蔵" "浅草" "品川" "村山" "多摩" "青梅" "高尾" "武蔵野" "奥多摩" "鹿角")
-    local NO=$(($RANDOM%${#automaton_names[@]}))
-    
-    local talk_cmd="${HOME}/bin/atalk.sh"
-    
-    if [ "${automaton_names[$NO]}" = "鹿角" ]; then
-        local automaton_name="${automaton_names[$NO]}"
-    else
-        local automaton_name="〝${automaton_names[$NO]}〟" # *** IMPORTANT ***
-        if [ "${automaton_names[$NO]}" = "奥多摩" ]; then
-            local oN=$(($RANDOM%2))
-            
-            if [ "${oN}" -eq 0 ]; then
-                local automaton_name="たまちゃん"
-            fi
-        fi
+  local status=$(echo ${PIPESTATUS[@]})
+  local status_array=($(echo $status))
+  local hist=$(history | tail -10 | awk '{$1=""; print $0}' | awk '{sub(/^[ \t]+/, "")}1')
+
+  local automaton_names=("武蔵" "浅草" "品川" "村山" "多摩" "青梅" "高尾" "武蔵野" "奥多摩" "鹿角")
+  local NO=$(($RANDOM%${#automaton_names[@]}))
+
+  local talk_cmd="${HOME}/bin/atalk.sh"
+
+  if [ "${automaton_names[$NO]}" = "鹿角" ]; then
+    local automaton_name="${automaton_names[$NO]}"
+  else
+    local automaton_name="〝${automaton_names[$NO]}〟" # *** IMPORTANT ***
+    if [ "${automaton_names[$NO]}" = "奥多摩" ]; then
+      local oN=$(($RANDOM%2))
+
+      if [ "${oN}" -eq 0 ]; then
+        local automaton_name="たまちゃん"
+      fi
     fi
-    
-    # diff openssl
-    OSKERN="$(uname -s)"
-    if [ ${OSKERN} = "Linux" ]; then
-        local prefix1="$(echo -n ${USER}$(tty) | openssl dgst -sha1 | awk '{print $2}')"
-        local new="musashi-${prefix1}-n"
-        local old="musashi-${prefix1}-o"
-        echo "${hist}" | openssl dgst -sha1 | awk '{print $2}' >"${user_tmp_dir}/${new}"
-    else
-        local prefix1="$(echo -n ${USER}$(tty) | openssl dgst -sha1)"
-        local new="musashi-${prefix1}-n"
-        local old="musashi-${prefix1}-o"
-        echo "${hist}" | openssl dgst -sha1 >"${user_tmp_dir}/${new}"
-    fi
-    
-    # check permission
-    $(check_perm ${user_tmp_dir}/${new})
-    
-    # playfulness
-    DISTRO="$(detect_distro)"
-    automaton_name="${automaton_name}(${DISTRO})"
-    
-    # Check command history
-    if cmp -s ${user_tmp_dir}/{${new},${old}} || test ! -e ${user_tmp_dir}/${old}; then
-        :
-    else
-        # Head of transcript
-        pcmd=$(echo "${hist}" | tail -1) # important " position
-        echo -n "${automaton_name}: 終了コードは"
-        transcript="終了コードは"
-        
-        # Middle of transcript, case by exit status code
-        if [ ${#status_array[@]} -gt 1 ]; then
-            echo "それぞれ"
-            i=1
-            
-            for s in ${status}; do
-                if [ ${#status_array[@]} -eq $i ]; then
-                    local echoopt="-ne"
-                else
-                    local echoopt="-e"
-                fi
-                
-                code_result=$(code_condition ${s})
-                echo ${echoopt} "${i} 番目、${s} です。${code_result}"
-                
-                i=$((${i}+1))
-            done
+  fi
+
+  # diff openssl
+  OSKERN="$(uname -s)"
+  if [ ${OSKERN} = "Linux" ]; then
+    local prefix1="$(echo -n ${USER}$(tty) | openssl dgst -sha1 | awk '{print $2}')"
+    local new="musashi-${prefix1}-n"
+    local old="musashi-${prefix1}-o"
+    echo "${hist}" | openssl dgst -sha1 | awk '{print $2}' >"${user_tmp_dir}/${new}"
+  else
+    local prefix1="$(echo -n ${USER}$(tty) | openssl dgst -sha1)"
+    local new="musashi-${prefix1}-n"
+    local old="musashi-${prefix1}-o"
+    echo "${hist}" | openssl dgst -sha1 > "${user_tmp_dir}/${new}"
+  fi
+
+  # check permission
+  $(check_perm ${user_tmp_dir}/${new})
+
+  # playfulness
+  # DISTRO="$(detect_distro)"
+  # automaton_name="${automaton_name}(${DISTRO})"
+
+  # Check command history
+  if cmp -s ${user_tmp_dir}/{${new},${old}} || test ! -e ${user_tmp_dir}/${old}; then
+    :
+  else
+    # Head of transcript
+    pcmd=$(echo "${hist}" | tail -1) # important " position
+    echo -n "${automaton_name}: 終了コードは"
+    transcript="終了コードは"
+
+    # Middle of transcript, case by exit status code
+    if [ ${#status_array[@]} -gt 1 ]; then
+      echo "それぞれ"
+      i=1
+
+      for s in ${status}; do
+        if [ ${#status_array[@]} -eq $i ]; then
+          local echoopt="-ne"
         else
-            code_result=$(code_condition ${status})
-            echo -n " ${status} です。${code_result}"
-            transcript="${transcript} ${status} です。${code_result}"
-        fi
-        
-        # End of transcript
-        if [[ $(echo "${automaton_name}" | grep "鹿角") ]]; then
-            echo "" # End of option "-n"
-        else
-            echo "――以上"
-            transcript="${transcript} 以上"
+          local echoopt="-e"
         fi
 
-        # for say or aquestalk.
-#        if [ -f /usr/bin/say ]; then
-#            say -v Kyoko -r 200 "${transcript}"
-#        elif [ -f ${talk_cmd} ]; then
-#            ${talk_cmd} -s 140 "${transcript}"
-#        fi
-    
+        code_result=$(code_condition ${s})
+        echo ${echoopt} "${i} 番目、${s} です。${code_result}"
+        
+        i=$((${i}+1))
+      done
+    else
+      code_result=$(code_condition ${status})
+      echo -n " ${status} です。${code_result}"
+      transcript="${transcript} ${status} です。${code_result}"
     fi
-    
-    cp "${user_tmp_dir}"/{"${new}","${old}"}
+
+    # End of transcript
+    if [[ $(echo "${automaton_name}" | grep "鹿角") ]]; then
+        echo "" # End of option "-n"
+    else
+        echo "――以上"
+        transcript="${transcript} 以上"
+    fi
+
+    # for say or aquestalk.
+    # if [ -f /usr/bin/say ]; then
+    #   say -v Kyoko -r 200 "${transcript}"
+    # elif [ -f ${talk_cmd} ]; then
+    #   ${talk_cmd} -s 140 "${transcript}"
+    # fi
+  fi
+
+  cp "${user_tmp_dir}"/{"${new}","${old}"}
 }
 PROMPT_COMMAND='musashi;'${PROMPT_COMMAND//musashi;/}
